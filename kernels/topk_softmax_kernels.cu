a6dd14ebad386582099638174d2ec2c15aa75c55
Gyuseong Lee <81148183+gseonglee@users.noreply.github.com>
2025-06-07 23:54:45 +0900

    fix(topk_softmax_kernels): add `#include <cfloat>` to fix `FLT_MAX` error
    * fix build error: 
    kernels/topk_softmax_kernels.cu(67): error: identifier "FLT_MAX" is undefined

--- kernels/topk_softmax_kernels.cu
+++ kernels/topk_softmax_kernels.cu
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <cfloat>
 
 #ifndef USE_ROCM
     #include <cub/util_type.cuh>
